ac4ab4f4e7efd9f596320fbe156f70c4fec025fc
Rimo <113169310+Rivmun@users.noreply.github.com>
2023-07-04 17:32:35 +0800

    Add files via upload

---
+++ fabric/src/main/java/com/rimo/tbim/FabricMod.java
@@ -0,0 +1,10 @@
+package com.rimo.tbim;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FabricMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        ModMain.init();
+    }
+}
