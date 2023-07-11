90a6571e11670c9f5463a8dbf71a1e4ae5248d97
Rimo <113169310+Rivmun@users.noreply.github.com>
2023-07-11 11:25:04 +0800

    1.0.1 (#2)
    - Fix dependence.
    - Allows you Shift + Right Click boat or minecart to unloaded mobs from it.
      - Prevent bounding box of little mob covered by vehicle and you cannot click to it.

--- common/src/main/java/com/rimo/tbim/ModMain.java
+++ common/src/main/java/com/rimo/tbim/ModMain.java
@@ -3,14 +3,22 @@ package com.rimo.tbim;
 import dev.architectury.event.EventResult;
 import dev.architectury.event.events.common.InteractionEvent;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.vehicle.AbstractMinecartEntity;
+import net.minecraft.entity.vehicle.BoatEntity;
 
 public class ModMain {
 	public static void init() {
 		InteractionEvent.INTERACT_ENTITY.register((player, entity, hand) -> {
-			if (!player.isSpectator() && player.isSneaking() && entity instanceof LivingEntity && entity.hasVehicle()) {
-				entity.stopRiding();
-				player.swingHand(hand);
-				return EventResult.interruptTrue();
+			if (!player.isSpectator() && player.isSneaking()) {
+				if (entity instanceof LivingEntity && entity.hasVehicle()) {
+					entity.stopRiding();
+					player.swingHand(hand);
+					return EventResult.interruptTrue();
+				} else if ((entity instanceof AbstractMinecartEntity || entity instanceof BoatEntity) && entity.hasPassengers()) {
+					entity.removeAllPassengers();
+					player.swingHand(hand);
+					return EventResult.interruptTrue();
+				}
             }
 			return EventResult.pass();
 		});
